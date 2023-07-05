a83ee98487c83513164c6afd74886005931d4bc7
olmiave <micaela.oliva@mail.utdt.edu>
2023-07-05 08:45:06 -0300

    kljsehfrdsyedrf

--- barras_fede.js
+++ barras_fede.js
@@ -1,50 +1,55 @@
 var trace1 = {
-    x: ["Rock", "Punk", "Metal"],
-    y: [28.53, 0.85, 0],
-    name: 'Federico',
-    marker: {color: '#F4D179' },
-    type: 'bar'
-  };
-  
-  var trace2 = {
-    x: ["Rock", "Punk", "Metal"],
-    y: [82, 51, 48],
-    name: 'Otros INTP',
-    marker: {color: '#cccccc' },
-    type: 'bar'
-  };
-  
-  var data = [trace1, trace2];
-  
-  var layout = {
-    // title: 'US Export of Plastic Scrap',
-    xaxis: {tickfont: {
+  x: ["Rock", "Punk", "Metal"],
+  y: [28.53, 0.85, 0],
+  name: 'Federico',
+  marker: {color: '#f0bf46' },
+  type: 'bar'
+};
+
+var trace2 = {
+  x: ["Rock", "Punk", "Metal"],
+  y: [82, 51, 48],
+  name: 'Otros INTP',
+  marker: {color: '#949494' },
+  type: 'bar'
+};
+
+var data = [trace1, trace2];
+
+var layout = {
+  // title: 'US Export of Plastic Scrap',
+  xaxis: {
+    tickfont: {
       size: 14,
       color: 'rgb(107, 107, 107)'
-    }},
-    yaxis: {
-      title: 'Proporción de escucha del género (%)',
-      titlefont: {
-        size: 16,
-        color: 'rgb(107, 107, 107)'
-      },
-      tickfont: {
-        size: 14,
-        color: 'rgb(107, 107, 107)'
-      }
+    }
+  },
+  yaxis: {
+    title: 'Proporción de escucha del género (%)',
+    titlefont: {
+      size: 16,
+      color: 'rgb(107, 107, 107)'
     },
-    legend: {
-      x: 0,
-      y: 1.2,
-      bgcolor: 'rgba(255, 255, 255, 0)',
-      bordercolor: 'rgba(255, 255, 255, 0)'
-    },
-    barmode: 'group',
-    bargap: 0.15,
-    bargroupgap: 0.1
-  };
-  
-  Plotly.newPlot('graficofede', data, layout);
+    tickfont: {
+      size: 14,
+      color: 'rgb(107, 107, 107)'
+    }
+  },
+  legend: {
+    x: 0,
+    y: 1.2,
+    bgcolor: 'rgba(255, 255, 255, 0)',
+    bordercolor: 'rgba(255, 255, 255, 0)'
+  },
+  barmode: 'group',
+  bargap: 0.15,
+  bargroupgap: 0.1,
+  plot_bgcolor: 'rgba(0, 0, 0, 0)', // Set the plot background color to transparent
+  paper_bgcolor: 'rgba(0, 0, 0, 0)' // Set the paper background color to transparent
+};
+
+Plotly.newPlot('graficofede', data, layout);
+
   
 // #fc8d59
 // #d73027
--- barras_mica.js
+++ barras_mica.js
@@ -1,51 +1,51 @@
 var trace1 = {
-    x: ["K-pop", "Jazz", "Electronic"],
-    y: [43.80, 1.80, 0.56],
-    name: 'Micaela',
-    marker: {color: '#EE9C9B' },
-    type: 'bar'
-  };
-  
-  var trace2 = {
-    x: ["K-pop", "Jazz", "Electronic"],
-    y: [52, 62, 75],
-    name: 'Otros ENFP',
-    marker: {color: '#cccccc' },
-    type: 'bar'
-  };
-  
-  var data = [trace1, trace2];
-  
-  var layout = {
-    // title: 'US Export of Plastic Scrap',
-    xaxis: {tickfont: {
+  x: ["K-pop", "Jazz", "Electronic"],
+  y: [43.80, 1.80, 0.56],
+  name: 'Micaela',
+  marker: {color: '#EB7F7D' },
+  type: 'bar'
+};
+
+var trace2 = {
+  x: ["K-pop", "Jazz", "Electronic"],
+  y: [52, 62, 75],
+  name: 'Otros ENFP',
+  marker: {color: '#949494' },
+  type: 'bar'
+};
+
+var data = [trace1, trace2];
+
+var layout = {
+  // title: 'US Export of Plastic Scrap',
+  xaxis: {
+    tickfont: {
       size: 14,
       color: 'rgb(107, 107, 107)'
-    }},
-    yaxis: {
-      title: 'Proporción de escucha del género (%)',
-      titlefont: {
-        size: 16,
-        color: 'rgb(107, 107, 107)'
-      },
-      tickfont: {
-        size: 14,
-        color: 'rgb(107, 107, 107)'
-      }
+    }
+  },
+  yaxis: {
+    title: 'Proporción de escucha del género (%)',
+    titlefont: {
+      size: 16,
+      color: 'rgb(107, 107, 107)'
     },
-    legend: {
-      x: 0,
-      y: 1.2,
-      bgcolor: 'rgba(255, 255, 255, 0)',
-      bordercolor: 'rgba(255, 255, 255, 0)'
-    },
-    barmode: 'group',
-    bargap: 0.15,
-    bargroupgap: 0.1
-  };
-  
-  Plotly.newPlot('graficomica', data, layout);
-  
-// #fc8d59
-// #d73027
-// #7f0000
+    tickfont: {
+      size: 14,
+      color: 'rgb(107, 107, 107)'
+    }
+  },
+  legend: {
+    x: 0,
+    y: 1.2,
+    bgcolor: 'rgba(255, 255, 255, 0)',
+    bordercolor: 'rgba(255, 255, 255, 0)'
+  },
+  barmode: 'group',
+  bargap: 0.15,
+  bargroupgap: 0.1,
+  plot_bgcolor: 'rgba(0, 0, 0, 0)', // Set the plot background color to transparent
+  paper_bgcolor: 'rgba(0, 0, 0, 0)' // Set the paper background color to transparent
+};
+
+Plotly.newPlot('graficomica', data, layout);
